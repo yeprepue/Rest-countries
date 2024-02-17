258b11bc0be7615600d2724df52845b347cad78c
yeprepue <yepretel@misena.edu.co>
2024-02-17 14:04:23 -0500

    selectedRegion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/countrry';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa'|'Americas'|'Asia'|'Europe'|'Oceania';
+
 @Component({
   selector: 'by-region',
   templateUrl: './by-region-page.component.html',
@@ -11,10 +13,15 @@ import { CountriesService } from '../../services/countries.service';
 export class ByRegionPageComponent {
 
   public countries: Country[] = [];
+  public regions: Region[] =['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion?:Region;
 
   constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(region: string): void {
+
+
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
     this.countriesService.searchRegion(region).subscribe( countries => {
       this.countries = countries;
     });
